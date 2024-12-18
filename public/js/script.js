2bbf66a41d85b38208df2ff73d363c52620421eb
sangah323 <choa323@naver.com>
2024-12-18 18:23:15 +0900

    feat : 버튼 클릭 시 메시지 표시 기능 추가

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+const contentBox = document.querySelector(".content-box");
 const run = document.querySelector(
   "#content > .content-box > ul > li:nth-of-type(3)"
 );
@@ -6,3 +7,14 @@ run.addEventListener("click", function colorChange() {
   run.innerHTML = "호다닥다ㅏㄱ닥ㄷ다갇";
   run.style.color = "red";
 });
+
+//
+const runBtn = document.createElement("button");
+runBtn.innerHTML = "과제 끝?";
+
+runBtn.addEventListener("click", function textChange() {
+  runBtn.innerHTML = "집으로 뛰어갓-!";
+  runBtn.style.color = "red";
+});
+
+contentBox.append(runBtn);
